c7bb2217a54081d9d89355586c879e48a4511ef8
nelicode <nelida11colque22@gmail.com>
2026-05-26 16:06:06 -0500

    Commit

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("Select a choice from the menu: ");
+
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+
+                string answer = Console.ReadLine();
+
+                DateTime currentTime = DateTime.Now;
+
+                Entry entry = new Entry();
+
+                entry._date = currentTime.ToShortDateString();
+                entry._promptText = prompt;
+                entry._entryText = answer;
+
+                journal.AddEntry(entry);
+            }
+
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+
+            else if (choice == 3)
+            {
+                Console.Write("Enter filename: ");
+                string filename = Console.ReadLine();
+
+                journal.SaveToFile(filename);
+            }
+
+            else if (choice == 4)
+            {
+                Console.Write("Enter filename: ");
+                string filename = Console.ReadLine();
+
+                journal.LoadFromFile(filename);
+            }
+
+            else if (choice == 5)
+            {
+                Console.WriteLine("Goodbye!");
+            }
+        }
     }
 }
